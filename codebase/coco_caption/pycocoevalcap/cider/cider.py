d3e758a45ff2aaeff4e8445867427a24198ebac8
Mikhail(omega) <mkorchevskiy@ryerson.ca>
2021-01-14 13:29:43 -0500

    gitignore is the dumbest shit in the world

---
+++ codebase/coco_caption/pycocoevalcap/cider/cider.py
@@ -0,0 +1,110 @@
+# Filename: cider.py
+#
+# Description: Describes the class to compute the CIDEr (Consensus-Based Image Description Evaluation) Metric
+#               by Vedantam, Zitnick, and Parikh (http://arxiv.org/abs/1411.5726)
+#
+# Creation Date: Sun Feb  8 14:16:54 2015
+#
+# Authors: Ramakrishna Vedantam <vrama91@vt.edu> and Tsung-Yi Lin <tl483@cornell.edu>
+
+from .cider_scorer import CiderScorer
+import pdb
+from pathlib import Path
+import os
+import sys
+import numpy as np
+
+# Use cPickle if it is installed, as it is much faster
+if 'cPickle' in sys.modules:
+    import cPickle as pickle
+else:
+    import pickle
+
+
+class Cider:
+    """
+    Main Class to compute the CIDEr metric
+
+    """
+    def __init__(self, df, test=None, refs=None, n=4, sigma=6.0):
+        # set cider to sum over 1 to 4-grams
+        self._n = n
+        # set the standard deviation parameter for gaussian penalty
+        self._sigma = sigma
+        self._dfMode = df
+        self.cider_scorer = CiderScorer(n=self._n, sigma=self._sigma)
+        #### move doc frequency load outside of scorer to make things faster
+        p = Path(__file__).parents[2]
+        #### HAVE TO MODIFY TO UNPICKLE STUFF PICKLED WITH PYTHON 2
+        if df != 'custom' and df != 'corpus':
+            f = open(os.path.join(p, 'data', df + '.p'), 'rb')
+            docFreq = pickle.load(f, encoding="latin1")
+
+            self._docFreq = docFreq['df']
+            self._ref_len = np.log(float(docFreq['ref_len']))
+        else: 
+            self._docFreq = None
+            self._ref_len = None
+    
+    def prepare_df(self, gts):
+        print('preparing df...')
+#         assert(gts.keys() == res.keys())
+        imgIds = gts.keys()
+
+        for id in imgIds:
+#             hypo = res[id]
+            ref = gts[id]
+            
+            
+#             print('hypo {}: {}'.format(id, hypo))
+#             print('ref {}: {}'.format(id, ref))
+            
+            # Sanity check.
+#             assert(type(hypo) is list)
+#             assert(len(hypo) == 1)
+            assert(type(ref) is list)
+            assert(len(ref) > 0)
+
+            self.cider_scorer.add_for_df((None, ref))
+        self.cider_scorer.compute_doc_freq()
+        self.cider_scorer.refresh_sets()
+        print('df ready...')
+    def compute_score(self, gts, res):
+        """
+        Main function to compute CIDEr score
+        :param  hypo_for_image (dict) : dictionary with key <image> and value <tokenized hypothesis / candidate sentence>
+                ref_for_image (dict)  : dictionary with key <image> and value <tokenized reference sentence>
+        :return: cider (float) : computed CIDEr score for the corpus
+        """
+        
+#         if self._dfMode == 'custom':
+#             self.prepare_df(gts, res)
+        
+        assert(gts.keys() == res.keys())
+        imgIds = gts.keys()
+
+#         cider_scorer = CiderScorer(n=self._n, sigma=self._sigma)
+        
+        print('adding data...', end="")
+        for id in imgIds:
+            hypo = res[id]
+            ref = gts[id]
+            
+#             print('hypo {}: {}'.format(id, hypo))
+#             print('ref {}: {}'.format(id, ref))
+            
+            # Sanity check.
+            assert(type(hypo) is list)
+            assert(len(hypo) == 1)
+            assert(type(ref) is list)
+            assert(len(ref) > 0)
+
+            self.cider_scorer += (hypo[0], ref, id)
+        print('done')
+        
+        (score, scores) = self.cider_scorer.compute_score(self._dfMode, self._docFreq, self._ref_len)
+
+        return score, scores
+
+    def method(self):
+        return "CIDEr"
---
+++ codebase/coco_caption/pycocoevalcap/eval.py
@@ -0,0 +1,87 @@
+__author__ = 'vrama91'
+
+import os
+import json
+
+from collections import defaultdict
+from .tokenizer.ptbtokenizer import PTBTokenizer
+# from .bleu.bleu import Bleu
+# from .meteor.meteor import Meteor
+# from .rouge.rouge import Rouge
+from .cider.cider import Cider
+
+class COCOEvalCap:
+    def __init__(self, pathToData, refName, candName, dfMode = "corpus"):
+        """
+        Reference file: list of dict('image_id': image_id, 'caption': caption).
+        Candidate file: list of dict('image_id': image_id, 'caption': caption).
+        :params: refName : name of the file containing references
+        :params: candName: name of the file containing cnadidates
+        """
+        self.eval = {}
+        self._refName = refName
+        self._candName = candName
+        self._pathToData = pathToData
+        self._dfMode = dfMode
+
+    def evaluate(self):
+        """
+        Load the sentences from json files
+        """
+        def readJson(refName, candName):
+
+            path_to_ref_file = os.path.join(self._pathToData, refName)
+            path_to_cand_file = os.path.join(self._pathToData, candName)
+
+            ref_list = json.loads(open(path_to_ref_file, 'r').read())
+            cand_list = json.loads(open(path_to_cand_file, 'r').read())
+
+            gts = defaultdict(list)
+            res = defaultdict(list)
+            # change of naming convention from ref to gts
+            for l in ref_list:
+                gts[l['image_id']].append({"caption": l['caption']})
+
+            # change of naming convention from cand to res
+            for l in cand_list:
+                res[l['image_id']].append({"caption": l['caption']})
+
+            return gts, res
+
+        print('Loading Data...')
+        gts, res = readJson(self._refName, self._candName)
+        # =================================================
+        # Set up scorers
+        # =================================================
+        print('tokenization...')
+        tokenizer = PTBTokenizer()
+        gts  = tokenizer.tokenize(gts)
+        res = tokenizer.tokenize(res)
+        
+        
+        # =================================================
+        # Set up scorers
+        # =================================================
+        print('setting up scorers...')
+        scorers = [
+#             (Bleu(4), ["Bleu_1", "Bleu_2", "Bleu_3", "Bleu_4"]),
+#             (Meteor(),"METEOR"),
+#             (Rouge(), "ROUGE_L"),
+            (Cider(self._dfMode), "CIDEr")
+        ]
+
+        # =================================================
+        # Compute scores
+        # =================================================
+        for scorer, method in scorers:
+            print('computing %s score...'%(scorer.method()))
+            score, scores = scorer.compute_score(gts, res)
+            if type(method) == list:
+                for sc, scs, m in zip(score, scores, method):
+                    self.setEval(sc, m)
+            else:
+                self.setEval(score, method)
+
+    def setEval(self, score, method):
+        self.eval[method] = score
+
